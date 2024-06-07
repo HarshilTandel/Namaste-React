f116b0934022baa635a77530874b7d4f714d733f
HarshilTandel <harshil@github.com>
2024-06-07 23:39:14 +0530

    first commit

---
+++ Chapter-1 Inception/Coding/app.js
@@ -0,0 +1,31 @@
+// const heading = React.createElement("h1", {
+//     id:"heading",
+//     xyz:"abc",
+// }, "Hello World From React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// ---------------------------------------------------->
+
+// const parent=React.createElement("div",{id:"parent"},
+
+// React.createElement("div",{id:"child"},
+// React.createElement("h1",{},"Hi I'm H1 Tag")
+// )
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+// ---------------------------------------------------->
+
+// const parent = React.createElement("div", { id: "parent" },
+//     [React.createElement("div", { id: "child1" },
+//         [React.createElement("h1", {}, "Hi,I'm H1 Tag from child 1"),
+//         React.createElement("h2", {}, "Hi,I'm H2 Tag from child 1")]
+//     )],
+//     [React.createElement("div", { id: "child2" },
+//     [React.createElement("h1", {}, "Hi,I'm H1 Tag from child 2"),
+//     React.createElement("h2", {}, "Hi,I'm H2 Tag from child 2")]
+// )]
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
